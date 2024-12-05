8efa33f91f12549d8f91637c7d6901247b8c82eb
maxregnerklos <135330858+maxregnerklos@users.noreply.github.com>
2024-12-04 23:24:53 -0800

    Update CLIOptions.cs

--- src/Applications/UUPDownload/CLIOptions.cs
+++ src/Applications/UUPDownload/CLIOptions.cs
@@ -22,185 +22,100 @@
 using CommandLine;
 using UnifiedUpdatePlatform.Services.WindowsUpdate;
 
-namespace UUPDownload
+namespace UUPDownload;
+
+[Verb("request-download", isDefault: true, HelpText = "Request a download from zero using a number of different request parameters.")]
+internal class DownloadRequestOptions
 {
-    [Verb("request-download", isDefault: true, HelpText = "Request a download from zero using a number of different request parameters.")]
-    internal class DownloadRequestOptions
-    {
-        [Option('s', "reporting-sku", HelpText = "The sku to report to the Windows Update servers. Example: Professional", Required = true)]
-        public OSSkuId ReportingSku
-        {
-            get; set;
-        }
+    [Option('s', "reporting-sku", Required = true, HelpText = "The sku to report to the Windows Update servers. Example: Professional")]
+    public required OSSkuId ReportingSku { get; init; }
 
-        [Option('v', "reporting-version", HelpText = "The version to report to the Windows Update servers. Example: 10.0.20152.1000", Required = true)]
-        public string ReportingVersion
-        {
-            get; set;
-        }
+    [Option('v', "reporting-version", Required = true, HelpText = "The version to report to the Windows Update servers. Example: 10.0.20152.1000")]
+    public required string ReportingVersion { get; init; }
 
-        [Option('t', "machine-type", HelpText = "The architecture to report to the Windows Update servers. Example: amd64", Required = true)]
-        public MachineType MachineType
-        {
-            get; set;
-        }
+    [Option('t', "machine-type", Required = true, HelpText = "The architecture to report to the Windows Update servers. Example: amd64")]
+    public required MachineType MachineType { get; init; }
 
-        [Option('r', "flight-ring", HelpText = "The ring to report to the Windows Update servers. Example: Retail, other example: External or Internal", Required = true)]
-        public string FlightRing
-        {
-            get; set;
-        }
+    [Option('r', "flight-ring", Required = true, HelpText = "The ring to report to the Windows Update servers. Example: Retail, other example: External or Internal")]
+    public required string FlightRing { get; init; }
 
-        [Option('c', "current-branch", HelpText = "The branch to report to the Windows Update servers. Example: 19h1_release", Required = true)]
-        public string CurrentBranch
-        {
-            get; set;
-        }
+    [Option('c', "current-branch", Required = true, HelpText = "The branch to report to the Windows Update servers. Example: 19h1_release")]
+    public required string CurrentBranch { get; init; }
 
-        [Option('b', "flighting-branch-name", HelpText = "The flighting branch name to report to the Windows Update servers. Example: Retail, other example: CanaryChannel, Dev, Beta or ReleasePreview", Required = false, Default = "")]
-        public string FlightingBranchName
-        {
-            get; set;
-        }
+    [Option('b', "flighting-branch-name", Required = false, Default = "", HelpText = "The flighting branch name to report to the Windows Update servers. Example: Retail, other example: CanaryChannel, Dev, Beta or ReleasePreview")]
+    public string FlightingBranchName { get; init; } = string.Empty;
 
-        [Option('y', "sync-current-version-only", HelpText = "Only get updates for the current version, enables getting cumulative updates.", Required = false, Default = false)]
-        public bool SyncCurrentVersionOnly
-        {
-            get; set;
-        }
+    [Option('y', "sync-current-version-only", Required = false, Default = false, HelpText = "Only get updates for the current version, enables getting cumulative updates.")]
+    public bool SyncCurrentVersionOnly { get; init; }
 
-        [Option('a', "branch-readiness-level", HelpText = "The branch readiness level to report to the Windows Update servers. Example: CB", Required = false, Default = "CB")]
-        public string BranchReadinessLevel
-        {
-            get; set;
-        }
+    [Option('a', "branch-readiness-level", Required = false, Default = "CB", HelpText = "The branch readiness level to report to the Windows Update servers. Example: CB")]
+    public string BranchReadinessLevel { get; init; } = "CB";
 
-        [Option('o', "output-folder", HelpText = "The folder to use for downloading the update files.", Required = false, Default = ".")]
-        public string OutputFolder
-        {
-            get; set;
-        }
+    [Option('o', "output-folder", Required = false, Default = ".", HelpText = "The folder to use for downloading the update files.")]
+    public string OutputFolder { get; init; } = ".";
 
-        [Option('e', "edition", HelpText = "The edition to get. Must be used with the language parameter. Omit either of these to download everything. Example: Professional", Required = false, Default = "")]
-        public string Edition
-        {
-            get; set;
-        }
+    [Option('e', "edition", Required = false, Default = "", HelpText = "The edition to get. Must be used with the language parameter. Omit either of these to download everything. Example: Professional")]
+    public string Edition { get; init; } = string.Empty;
 
-        [Option('l', "language", HelpText = "The language to get. Must be used with the edition parameter. Omit either of these to download everything. Example: en-US", Required = false, Default = "")]
-        public string Language
-        {
-            get; set;
-        }
+    [Option('l', "language", Required = false, Default = "", HelpText = "The language to get. Must be used with the edition parameter. Omit either of these to download everything. Example: en-US")]
+    public string Language { get; init; } = string.Empty;
 
-        [Option('z', "releasetype", HelpText = "The release type to report to the Windows Update servers. Example: Production", Required = false, Default = "Production")]
-        public string ReleaseType
-        {
-            get; set;
-        }
+    [Option('z', "releasetype", Required = false, Default = "Production", HelpText = "The release type to report to the Windows Update servers. Example: Production")]
+    public string ReleaseType { get; init; } = "Production";
 
-        [Option('n', "contenttype", HelpText = "The content type to report to the Windows Update servers. Example: Mainline, Custom", Required = false, Default = "Mainline")]
-        public string ContentType
-        {
-            get; set;
-        }
+    [Option('n', "contenttype", Required = false, Default = "Mainline", HelpText = "The content type to report to the Windows Update servers. Example: Mainline, Custom")]
+    public string ContentType { get; init; } = "Mainline";
 
-        [Option('m', "mail", HelpText = "Email for the Windows Insider account to use to generate authorization tokens (Optional)", Required = false, Default = "")]
-        public string Mail
-        {
-            get; set;
-        }
+    [Option('m', "mail", Required = false, Default = "", HelpText = "Email for the Windows Insider account to use to generate authorization tokens (Optional)")]
+    public string Mail { get; init; } = string.Empty;
 
-        [Option('p', "password", HelpText = "Password for the Windows Insider account to use to generate authorization tokens (If 2FA, must be generated app password) (Optional)", Required = false, Default = "")]
-        public string Password
-        {
-            get; set;
-        }
-    }
+    [Option('p', "password", Required = false, Default = "", HelpText = "Password for the Windows Insider account to use to generate authorization tokens (If 2FA, must be generated app password) (Optional)")]
+    public string Password { get; init; } = string.Empty;
+}
 
-    [Verb("replay-download", isDefault: false, HelpText = "Replay a download from zero using a *.uupmcreplay file.")]
-    internal class DownloadReplayOptions
-    {
-        [Option('r', "replay-metadata", HelpText = @"The path to a *.uupmcreplay file to replay an older update and resume the download process. Example: D:\20236.1005.uupmcreplay", Required = true)]
-        public string ReplayMetadata
-        {
-            get; set;
-        }
+[Verb("replay-download", isDefault: false, HelpText = "Replay a download from zero using a *.uupmcreplay file.")]
+internal class DownloadReplayOptions
+{
+    [Option('r', "replay-metadata", Required = true, HelpText = @"The path to a *.uupmcreplay file to replay an older update and resume the download process. Example: D:\20236.1005.uupmcreplay")]
+    public required string ReplayMetadata { get; init; }
 
-        [Option('t', "machine-type", HelpText = "The architecture to report to the Windows Update servers. Example: amd64", Required = true)]
-        public MachineType MachineType
-        {
-            get; set;
-        }
+    [Option('t', "machine-type", Required = true, HelpText = "The architecture to report to the Windows Update servers. Example: amd64")]
+    public required MachineType MachineType { get; init; }
 
-        [Option('o', "output-folder", HelpText = "The folder to use for downloading the update files.", Required = false, Default = ".")]
-        public string OutputFolder
-        {
-            get; set;
-        }
+    [Option('o', "output-folder", Required = false, Default = ".", HelpText = "The folder to use for downloading the update files.")]
+    public string OutputFolder { get; init; } = ".";
 
-        [Option('e', "edition", HelpText = "The edition to get. Must be used with the language parameter. Omit either of these to download everything. Example: Professional", Required = false, Default = "")]
-        public string Edition
-        {
-            get; set;
-        }
+    [Option('e', "edition", Required = false, Default = "", HelpText = "The edition to get. Must be used with the language parameter. Omit either of these to download everything. Example: Professional")]
+    public string Edition { get; init; } = string.Empty;
 
-        [Option('l', "language", HelpText = "The language to get. Must be used with the edition parameter. Omit either of these to download everything. Example: en-US", Required = false, Default = "")]
-        public string Language
-        {
-            get; set;
-        }
+    [Option('l', "language", Required = false, Default = "", HelpText = "The language to get. Must be used with the edition parameter. Omit either of these to download everything. Example: en-US")]
+    public string Language { get; init; } = string.Empty;
 
-        [Option("fixup", HelpText = @"Applies a fixup to files in output folder. Example: Appx", Required = false)]
-        public Fixup? Fixup
-        {
-            get; set;
-        }
+    [Option("fixup", Required = false, HelpText = @"Applies a fixup to files in output folder. Example: Appx")]
+    public Fixup? Fixup { get; init; }
 
-        [Option("appxroot", HelpText = @"The folder containing the appx files for use with the Appx fixup", Required = false)]
-        public string AppxRoot
-        {
-            get; set;
-        }
+    [Option("appxroot", Required = false, HelpText = @"The folder containing the appx files for use with the Appx fixup")]
+    public string? AppxRoot { get; init; }
 
-        [Option("cabsroot", HelpText = @"The folder containing the cab files for use with the Appx fixup", Required = false)]
-        public string CabsRoot
-        {
-            get; set;
-        }
-    }
+    [Option("cabsroot", Required = false, HelpText = @"The folder containing the cab files for use with the Appx fixup")]
+    public string? CabsRoot { get; init; }
+}
 
-    [Verb("get-builds", isDefault: false, HelpText = "Get builds in all rings matching the request type")]
-    internal class GetBuildsOptions
-    {
-        [Option('s', "reporting-sku", HelpText = "The sku to report to the Windows Update servers. Example: Professional", Required = true)]
-        public OSSkuId ReportingSku
-        {
-            get; set;
-        }
+[Verb("get-builds", isDefault: false, HelpText = "Get builds in all rings matching the request type")]
+internal class GetBuildsOptions
+{
+    [Option('s', "reporting-sku", Required = true, HelpText = "The sku to report to the Windows Update servers. Example: Professional")]
+    public required OSSkuId ReportingSku { get; init; }
 
-        [Option('t', "machine-type", HelpText = "The architecture to report to the Windows Update servers. Example: amd64", Required = true)]
-        public MachineType MachineType
-        {
-            get; set;
-        }
+    [Option('t', "machine-type", Required = true, HelpText = "The architecture to report to the Windows Update servers. Example: amd64")]
+    public required MachineType MachineType { get; init; }
 
-        [Option('m', "mail", HelpText = "Email for the Windows Insider account to use to generate authorization tokens (Optional)", Required = false, Default = "")]
-        public string Mail
-        {
-            get; set;
-        }
+    [Option('m', "mail", Required = false, Default = "", HelpText = "Email for the Windows Insider account to use to generate authorization tokens (Optional)")]
+    public string Mail { get; init; } = string.Empty;
 
-        [Option('p', "password", HelpText = "Password for the Windows Insider account to use to generate authorization tokens (If 2FA, must be generated app password) (Optional)", Required = false, Default = "")]
-        public string Password
-        {
-            get; set;
-        }
+    [Option('p', "password", Required = false, Default = "", HelpText = "Password for the Windows Insider account to use to generate authorization tokens (If 2FA, must be generated app password) (Optional)")]
+    public string Password { get; init; } = string.Empty;
 
-        [Option("preview-targeting-attribute", HelpText = "The name of the set of targeting attributes to use. (Optional, Preview)", Required = false, Default = "")]
-        public string TargetingAttribute
-        {
-            get; set;
-        }
-    }
+    [Option("preview-targeting-attribute", Required = false, Default = "", HelpText = "The name of the set of targeting attributes to use. (Optional, Preview)")]
+    public string TargetingAttribute { get; init; } = string.Empty;
 }
